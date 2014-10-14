2b590201443b56b268df91361b37614e2ab461aa
Johannes Frank <jmfrank63@gmail.com>
2014-10-14 21:34:30 +0000

    First commit

---
+++ hello-world.php
@@ -0,0 +1,10 @@
+<?php
+// A simple web site in Cloud9 that runs through Apache
+// Press the 'Run' button on the top to start the web server,
+// then click the URL that is emitted to the Output tab of the console
+
+echo 'Hello world from Cloud9!';
+$names = "Atlas, Perseus, Herkules";
+$name_list = explode(", ", $names);
+var_dump($IP);
+?>

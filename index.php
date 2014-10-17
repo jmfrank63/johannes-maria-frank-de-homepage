64bc51cd6eec80e06a847805ac3e0ed88bfcc07c
Johannes Frank <jmfrank63@gmail.com>
2014-10-17 13:41:40 +0000

    security commit

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-
 phpinfo();
 ?>
+<

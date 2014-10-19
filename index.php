a3a442cd49f8d34eb13d52f0f54a1791b4237828
Johannes Frank <jmfrank63@gmail.com>
2014-10-19 03:11:24 +0000

    Started Homepage

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Homepage of Johannes Maria Frank</title>
+    </head>
+    <body>
+        <h1>Welcome to my Homepage</h1>
+    </body>
+</html>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-echo $_SERVER['REQUEST_URI'];
-var_dump($_SERVER);
 ?>
-<

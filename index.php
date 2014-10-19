eeef4e81ccc3510d442d8065553d3c0db8c07684
Johannes Frank <jmfrank63@gmail.com>
2014-10-19 02:17:34 +0000

    echoed request_uri

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-echo $_SERVER['REQUEST_URI']
+echo $_SERVER['REQUEST_URI'];
+var_dump($_SERVER);
 ?>
 <

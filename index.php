767e5f4c93bf9518093a786eefe0b9fb38fc82cd
Johannes Frank <jmfrank63@gmail.com>
2014-11-28 22:40:34 +0000

    Checking pdo drivers

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-$home = file_get_contents("home.html");
-echo $home;
+//$home = file_get_contents("home.html");
+//echo $home;
+print_r(PDO::getAvailableDrivers());
 phpinfo();
 ?>

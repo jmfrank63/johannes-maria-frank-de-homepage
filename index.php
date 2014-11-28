752d0dcbd2c425e7b3ee113b2dd3fc3255759111
Johannes Frank <jmfrank63@gmail.com>
2014-11-28 20:35:57 +0000

    gm extension working, Font working

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Homepage of Johannes Maria Frank</title>
-    </head>
-    <body>
-        <h1>Welcome to my Homepage</h1>
-    </body>
-</html>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
+$home = file_get_contents("home.html");
+echo $home;
 phpinfo();
 ?>
